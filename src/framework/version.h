d4d78e5ab207225a9d4c5796d69f62eab4786948
Aoran Zeng <ccmywish@qq.com>
2026-08-20 07:09:34 +0800

    Prerelease `v0.2.6.3`

--- src/framework/version.h
+++ src/framework/version.h
@@ -8,11 +8,11 @@
  * 发布新版本前请修改此文件
  * ------------------------------------------------------------*/
 
-#define Chsrc_Version        "0.2.6.2"
+#define Chsrc_Version        "0.2.6.3"
 // 以下四个宏仅用于 resource/chsrc.rc
 #define Chsrc_Version_Major  0
 #define Chsrc_Version_Minor  2
 #define Chsrc_Version_Patch  6
-#define Chsrc_Version_Pre    2
+#define Chsrc_Version_Pre    3
 
-#define Chsrc_Release_Date   "2026/08/14"
+#define Chsrc_Release_Date   "2026/08/20"
